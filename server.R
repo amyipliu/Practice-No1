3f7edca4f3b6c49d165bd6fc29b4c0d966261248
amyipliu <ipl222@nyu.edu>
2017-07-16 20:17:01 -0400

    Add files via upload
    project update 0716

--- server.R
+++ server.R
@@ -32,4 +32,109 @@ shinyServer(function(input, output){
                                   height='auto'
       ))
   })
+  output$edpie = renderGvis({
+    cc1 %>% 
+      group_by(., educ) %>% 
+      select(., educ) %>%
+      filter(., educ %in% c('Graduate', 'Undergraduate', 'High School', 'Others')) %>% 
+      summarise(e_sum = n()) %>%
+      arrange(., desc(e_sum)) %>% 
+      gvisPieChart(., 'educ', 'e_sum', options = list(title="Card Users by Education",
+                                                      titleTextStyle="{color:'dark',fontName:'Courier',fontSize:16}"))
+  })
+  output$edpbar = renderGvis({
+    cc1 %>% 
+      group_by(., educ) %>% 
+      select(., educ) %>%
+      filter(., educ %in% c('Graduate', 'Undergraduate', 'High School', 'Others')) %>% 
+      summarise(e_sum = n()) %>%
+      arrange(., desc(e_sum)) %>%
+      gvisBarChart(., 'educ', 'e_sum', list(title="Card Users by Education",
+                                               titleTextStyle="{color:'dark',fontName:'Courier',fontSize:16}"))
+  })
+  output$ageppie = renderGvis({
+    cc1 %>% 
+      group_by(., ager) %>% 
+      summarise(aget = n()) %>%
+      gvisPieChart('ager', 'aget', list(title = 'Card Users by Age'))
+  })
+  output$agepbar = renderPlot({
+    cc1 %>% ggplot(aes(x = ager)) + 
+      geom_bar(aes(fill = ager)) + 
+      xlab('Age Range') +
+      scale_fill_discrete(name = 'Age Range')+
+      ggtitle('Card Users by Age')+
+      ylab('Count')
+  })
+  output$marppie = renderGvis({
+    cc1 %>% 
+      group_by(., marr) %>% 
+      summarise(Marriage = n()) %>% 
+      arrange(., desc(Marriage)) %>%
+      gvisPieChart('marr', 'Marriage', list(title = 'Card Users by Marriage', 
+                                            pieSliceText='label',
+                                            pieHole=0.3))
+  })
+  output$marpbar = renderGvis({
+    cc1 %>% 
+      group_by(., marr) %>% 
+      summarise(Marriage = n()) %>% 
+      arrange(., desc(Marriage)) %>%
+      gvisBarChart('marr', 'Marriage', list(title = 'Card Users by Marriage'))
+  })
+  output$lbsb = renderPlot({
+    boxplot(cc1$limit_bal2, main = 'Credit Limit Range')
+  })
+  output$lbgbx = renderPlot({
+    cc1 %>% 
+      ggplot() + 
+      geom_boxplot(aes( x= gender, y = limit_bal2, fill = gender))+
+      xlab('Gender') + ylab('Credit Limit')
+  })
+  output$test = renderGvis({
+    cc1 %>% group_by(., age) %>% 
+      select(., age) %>%
+      gvisHistogram()
+  })
+  output$edgepop = renderGvis({
+    cc1 %>% 
+      group_by(., educ) %>%
+      summarise(Male = sum(gender == 'M'), Female = sum(gender == 'F')) %>%
+      arrange(., desc(Female)) %>% 
+      gvisColumnChart(xvar = c('educ'), yvar = c('Male', 'Female'), options = list(fill = 'gender'))
+  })
+  output$ed_M = renderGvis({
+    cc1 %>% 
+      group_by(., educ) %>%
+      summarise(Male = sum(gender == 'M')) %>%
+      arrange(., desc(Male)) %>% 
+      gvisPieChart('educ', 'Male')
+  })
+  output$ed_F = renderGvis({
+    cc1 %>% 
+      group_by(., educ) %>%
+      summarise(Female = sum(gender == 'F')) %>%
+      arrange(., desc(Female)) %>% 
+      gvisPieChart('educ', numvar ='Female')
+  })
+  output$age_gender = renderGvis({
+    cc1 %>%
+      group_by(., ager) %>%
+      summarise(Female = sum(gender == 'F'), Male = sum(gender == 'M')) %>% 
+      gvisColumnChart('ager', c('Male', 'Female'))
+  })
+  output$lb_edu = renderPlot({
+    cc1 %>% 
+      ggplot(aes(x= educ, y = limit_bal2)) +  
+      geom_boxplot(aes(fill=educ)) + xlab('Education Level') + 
+      ylab('Credit Limit') +
+      scale_fill_brewer(palette = 'YlGnBu', name = 'Edu Level')
+  })
+  output$lb_mar = renderPlot({
+    cc1 %>% 
+      ggplot(aes(x = marr, y = limit_bal2)) +  
+      geom_boxplot(aes(fill=marr)) + xlab('Marital') + 
+      ylab('Credit Limit') +
+      scale_fill_brewer(palette = 'YlGnBu', name = 'Marital')
+  })
 })
--- ui.r
+++ ui.r
@@ -17,14 +17,60 @@ shinyUI(dashboardPage(skin = 'black',
       tabItem(tabName = 'home',
               'homepage'),
       tabItem(tabName = 'population',
+              fluidRow( #test
+                box(title = 'test', solidHeader = TRUE, htmlOutput('test')),
+                box(title = "Inputs", status = "warning", solidHeader = TRUE,
+                    "Box content here", br(), "More box content",
+                    sliderInput("slider", "Slider input:", 1, 10, 5)
+                )
+              ),
               fluidRow(
-                  column(width = 10,
-                         box(htmlOutput('plot1'))),
-                  column(width = 10,
-                         box(htmlOutput('gensum'))) 
-                  )),
+                box(htmlOutput('plot1'), width = 5, background = "light-blue"),
+                box(htmlOutput('gensum'), background = "maroon") 
+                ),
+              fluidRow(
+                box(htmlOutput('edpie'), width = 5, background = "light-blue"),
+                box(htmlOutput('edpbar'), background = "maroon")
+               ),
+              fluidRow(
+                box(htmlOutput('ageppie'), width = 5, height = 'auto', background = 'light-blue'),
+                box(plotOutput('agepbar'), background = 'maroon')
+                ),
+              fluidRow(
+                box(htmlOutput('marppie'), width = 5, background = 'light-blue'),
+                box(htmlOutput('marpbar'), background = 'maroon')
+                ),
+              fluidRow(
+                box(title = 'Male Education Portion', 
+                    htmlOutput('ed_M'), width = 5, 
+                    background = 'light-blue',
+                    solidHeader = T),
+                box(title = 'Female Education Portion', 
+                    htmlOutput('ed_F'), width = 5,
+                    background = 'maroon',
+                    solidHeader = T),
+                box(title = 'Education & Gender Distribution', 
+                    htmlOutput('edgepop'),
+                    background = 'teal',
+                    solidHeader = T),
+                box(title = 'Gender & Age Distribution',
+                    htmlOutput('age_gender'),
+                    background = 'teal',
+                    solidHeader = T)
+              )),
       tabItem(tabName = 'limb',
-              'limb analysis'),
+              fluidRow(
+                box(plotOutput('lbsb'), width = 6, background = 'light-blue')
+              ),
+              fluidRow(
+                box(plotOutput('lbgbx'), title = 'Credit Limit by Gender',
+                    background = 'light-blue')
+               ),
+              fluidRow(
+                box(plotOutput('lb_edu'), title = 'Credit Limit by Education', background = 'light-blue'),
+                box(plotOutput('lb_mar'), title = 'Credit Limit by Marital', background = 'light-blue')
+              )
+              ),
       tabItem(tabName = 'data',
               'data')
     )
